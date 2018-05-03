87f3ddb5c3f9f20aeb92e045565ef5acca851418
Jakob Høy <gatlingjakob@hotmail.com>
2018-05-03 15:22:39 +0200

    crud

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -44,19 +44,140 @@ namespace MentorOnlinev1.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-        // Create
+        // Create mentor
         [HttpGet]
-        public IActionResult Create()
+        public IActionResult CreateMentor()
         {
             return View();
         }
         [HttpPost]
-        public IActionResult Create(Mentor ment)
+        public IActionResult CreateMentor(Mentor ment)
         {
             db.Mentors.Add(ment);
             db.SaveChanges();
             return View();
         }
+
+        //Create student
+         [HttpGet]
+        public IActionResult CreateStudent()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateStudent(Student stud)
+        {
+            db.Students.Add(stud);
+            db.SaveChanges();
+            return View();
+        }
         
+        // search metode
+        // søg på - id
+        // få udskrevet alt info on den ene person
+        public IActionResult SearchMentor(int id = 1)
+        {
+            ViewBag.number = id;
+
+            ViewBag.ment = db.Mentors.ToList();
+
+            return View();
+        }
+
+         public IActionResult SearchStudent(int id = 1)
+        {
+            ViewBag.number = id;
+
+            ViewBag.stud = db.Students.ToList();
+
+            return View();
+        }
+
+    //Update Mentor
+
+     [HttpGet]
+        public IActionResult UpdateMentor(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult UpdateMentor(int id, string name, string password, string email, string subjects, int role, int online, string imgurl)
+        {
+            Mentor ment = new Mentor()
+            {
+                MentorId = id,
+                Name = name,
+                Password = password,
+                Email = email,
+                Subjects = subjects,
+                Role = role,
+                Online = online,
+                Imgurl = imgurl
+            };
+            db.Update(ment);
+            db.SaveChanges();
+            return View();
+        }
+
+    //Update Student
+
+    [HttpGet]
+        public IActionResult UpdateStudent(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult UpdateStudent(int id, string name, string password, string email, int subscribed, int role, int paymentinfo)
+        {
+            Student stud = new Student()
+            {
+                StudentId = id,
+                Name = name,
+                Password = password,
+                Email = email,
+                Subscribed = subscribed,
+                Role = role,
+                Paymentinfo = paymentinfo,
+            };
+            db.Update(stud);
+            db.SaveChanges();
+            return View();
+        }
+
+
+        //DeleteMentor
+
+         [HttpGet]
+        public IActionResult DeleteMentor()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DeleteMentor(int id)
+        {
+            Mentor ment = new Mentor() { MentorId = id };
+            db.Mentors.Remove(ment);
+            db.SaveChanges();
+            return View();
+        }
+
+          [HttpGet]
+        public IActionResult DeleteStudent()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DeleteStudent(int id)
+        {
+            Student stud = new Student() { StudentId = id };
+            db.Students.Remove(stud);
+            db.SaveChanges();
+            return View();
+        }
+
     }
 }
